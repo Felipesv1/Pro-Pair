970d5dfd0d5aee69635ca41f1c110f6216b61bcd
Felipe Pereira <104787971+Felipesv1@users.noreply.github.com>
2022-09-27 00:42:44 -0300

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import header from '../img/Header.png'
+import styled from 'styled-components'
+
+const Img = styled.img`
+width:100%;
+`
+export default function Header(){
+
+
+    return(
+        <section>
+        <div>
+            <Img src={header}alt='header'/>
+        </div>
+        <ul>
+            <li>RC</li>
+            <li>ABOUT</li>
+            <li>RECIPES</li>
+        </ul>
+        </section>
+    )
+}
