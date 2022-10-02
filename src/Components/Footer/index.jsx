72affc5520a2d834d984fb4afe3f6b5303102a03
Felipe Pereira <104787971+Felipesv1@users.noreply.github.com>
2022-10-02 00:04:10 -0300

    links dos Icones adicionados

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -11,10 +11,10 @@ export default function Footer(){
   return(
     <S.Footer>
       <S.BoxIcons>
-        <S.List><a href=""><img src={Insta} alt="icon Instagram" title=' icon Instagram'/> </a></S.List>
-        <S.List><a href=""><img src={twitter} alt="icon twitter" title=' icon twitter'/> </a></S.List>
-        <S.List><a href=""><img src={Face} alt="icon Facebook" title=' icon Facebook'/> </a></S.List>
-        <S.List><a href=""><img src={Pinterest} alt="icon Pinterest" title=' icon Pinterest'/> </a></S.List>
+        <S.List><a href="https://www.instagram.com/"><img src={Insta} alt="icon Instagram" title=' icon Instagram'/> </a></S.List>
+        <S.List><a href="https://www.twitter.com/"><img src={twitter} alt="icon twitter" title=' icon twitter'/> </a></S.List>
+        <S.List><a href="https://www.Facebook.com/"><img src={Face} alt="icon Facebook" title=' icon Facebook'/> </a></S.List>
+        <S.List><a href="https://www.Pinterest.com/"><img src={Pinterest} alt="icon Pinterest" title=' icon Pinterest'/> </a></S.List>
       </S.BoxIcons>
       <S.BoxList>
             <S.List>ABOUT</S.List>
